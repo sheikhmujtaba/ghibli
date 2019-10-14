b5b9b8890c8e76a7d35fa398c99b3bfff6189be1
Eric A. Bayer <eric.bayer@centre.edu>
2019-10-13 22:04:31 -0400

    work

--- ghibli.js
+++ ghibli.js
@@ -1,20 +1,50 @@
 var filmPromise = d3.json("https://ghibliapi.herokuapp.com/films")
 filmPromise.then(function(films)
 {
+    d3.select("h1")
+    .text("Select film")
 console.log("film", films);
     displayfilms(films)
+    displaydescription(films)
 },
 function(err)
 {
-console.log("fail", err)    
+console.log("fail", err)
+        d3.select("h1")
+    .text("No Films Found")
 })
 
 var displayfilms = function(films)
 {
-    var box = d3.select("#gib")
-    box.append("p")
-    .text(function(d){
-        return d.description;
-    }
-         )
+   d3.select("#gib")
+    .selectAll("h2")
+    .data(films)
+    .enter()
+    .append("h2")
+    .text(function(d)
+         {
+       return d.title
+   })
+
+}
+
+  var displaydescription = function(films)
+{
+   var box = 
+    d3.select("#gib2")
+    .selectAll("div")
+   .data(films)
+   .enter()
+    box.append("div")
+    .attr("class", "description")
+    .text(function(d)
+         {
+        return d.description
+    })
+    var info = 
+        box.append("p")
+    .text(function(d)
+         {
+        return d.title
+    })
 }
